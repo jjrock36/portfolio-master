af8e3ac97d29fdf26dbd191631f0d587aa6f3441
jjrock36 <68864949+jjrock36@users.noreply.github.com>
2021-06-24 16:19:08 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// const btns = document.querySelectorAll('.btn');
+// const prevBtn = document.querySelector('.prev');
+// const nextBtn = document.querySelector('.next');
+
+// btns.forEach((btn) => {
+//     btn.addEventListener('click', (e) => {
+//         const activeBtn = e.currentTarget.classList;
+//         if(activeBtn.contains('active-btn')) {
+//             return;
+//         }
+//     })
+// })
